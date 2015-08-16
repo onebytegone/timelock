22b3f06c8c23bc16e0199811962d9b0e66e8cdc2
Ethan Smith <ethan@onebytegone.com>
2015-08-15 20:42:04 -0400

    Added example config

---
+++ environment.demo.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set('America/Los_Angeles');
+
+// This is the base directory that the files should be stored in
+define('STORAGE_DIR', 'storage');
+
+// This is the file to store the file settings
+define('CONFIG_FILE', 'timelock.json');
